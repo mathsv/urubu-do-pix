614877c10e42cb6c35eab7d1caafc58bd2917693
Matheus Vieira <matheusvieira0101@gmail.com>
2023-09-09 20:59:19 -0300

    feat: add Optional findTransactionById

--- src/main/java/com/urubuDoPix/repositories/TransactionRepository.java
+++ src/main/java/com/urubuDoPix/repositories/TransactionRepository.java
@@ -1,9 +1,12 @@
 package com.urubuDoPix.repositories;
 
 import com.urubuDoPix.model.Transaction;
+import com.urubuDoPix.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface TransactionRepository extends JpaRepository<Transaction, UUID> {
+    Optional<Transaction> findTransactionById(UUID id);
 }
