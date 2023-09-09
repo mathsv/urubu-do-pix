0796a751ec3aabe810eb59c8402e5175aaf09abf
Matheus Vieira <matheusvieira0101@gmail.com>
2023-09-09 20:58:47 -0300

    feat: add GetTransaction and GetAllTransactions methods

--- src/main/java/com/urubuDoPix/services/TransactionService.java
+++ src/main/java/com/urubuDoPix/services/TransactionService.java
@@ -5,13 +5,17 @@ import com.urubuDoPix.model.Transaction;
 import com.urubuDoPix.model.User;
 import com.urubuDoPix.repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -50,5 +54,11 @@ public class TransactionService {
         }
     }
 
+    public Optional<Transaction> getTransaction(UUID id){
+        return this.transactionRepository.findTransactionById(id);
+    }
 
+    public List<Transaction> getAllTransactions(){
+        return this.transactionRepository.findAll();
+    }
 }
