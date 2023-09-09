adc052bd727c99065df0cfd904cd1dbf8c3f0109
Matheus Vieira <matheusvieira0101@gmail.com>
2023-09-09 16:43:48 -0300

    feat: create UserRepository

---
+++ src/main/java/com/urubuDoPix/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.urubuDoPix.repositories;
+
+import com.urubuDoPix.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findUserByDocument(String document);
+    Optional<User> findUserById(UUID id);
+}
