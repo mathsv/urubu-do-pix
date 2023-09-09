deba3d1ba9b3c91328ae51791a62854300455a77
Matheus Vieira <matheusvieira0101@gmail.com>
2023-09-09 13:18:01 -0300

    create config files

---
+++ src/main/java/com/urubuDoPix/UrubuDoPixApplication.java
@@ -0,0 +1,13 @@
+package com.urubuDoPix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrubuDoPixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrubuDoPixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/urubuDoPix/UrubuDoPixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urubuDoPix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrubuDoPixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
