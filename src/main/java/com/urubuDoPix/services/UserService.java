8d3c59e4c97446121e5ac20e667091d73fd37719
Matheus Vieira <matheusvieira0101@gmail.com>
2023-09-09 16:39:59 -0300

    feat: create UserService

---
+++ src/main/java/com/urubuDoPix/services/UserService.java
@@ -0,0 +1,32 @@
+package com.urubuDoPix.services;
+
+import com.urubuDoPix.dtos.UserDTO;
+import com.urubuDoPix.model.User;
+import com.urubuDoPix.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findUserById(UUID id){
+        return this.userRepository.findUserById(id).orElseThrow(()-> new Exception("Usuário não encontrado!"));
+    }
+    public void saveUser(User user){
+        this.userRepository.save(user);
+    }
+    public List<User> getAllUsers(){
+        return this.userRepository.findAll();
+    }
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+}
