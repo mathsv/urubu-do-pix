e08b525ae971194ae18457787355c5e4235adef6
Matheus Vieira <matheusvieira0101@gmail.com>
2023-09-09 21:00:01 -0300

    feat: add GetAllTransactions and GetTransactionById methods

--- src/main/java/com/urubuDoPix/controller/TransactionController.java
+++ src/main/java/com/urubuDoPix/controller/TransactionController.java
@@ -10,6 +10,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transactions")
@@ -22,5 +25,16 @@ public class TransactionController {
 
         Transaction newTransaction = this.transactionService.createTransaction(data);
         return new ResponseEntity<>(newTransaction, HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Transaction>> getAllTransactions(){
+        return new ResponseEntity<>(this.transactionService.getAllTransactions(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Transaction>> getTransactionById(@PathVariable UUID id){
+        Optional<Transaction> transaction = this.transactionService.getTransaction(id);
+        return new ResponseEntity<>(transaction, HttpStatus.OK);
     }
 }
